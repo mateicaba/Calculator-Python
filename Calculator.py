372af4036de240b38373cd6d6f293a5daad60b36
mateicaba <62711887+mateicaba@users.noreply.github.com>
2023-05-18 13:01:56 +0300

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,112 @@
+from tkinter import *
+#folosim tkinter pentru a avea aspect de calculator
+import parser
+from math import factorial
+
+
+root = Tk()
+root.title('Calculatorul lui Matei')
+#root.iconbitmap(path_to_your_icon) nu merge pentru sters logo-ul de la tkinter
+i = 0
+#intra aici dupa ce este apasata o tasta
+def get_variables(num):
+    global i
+    display.insert(i,num)
+    i+=1
+ 
+#aceasta functie afla rezultatul, sterge tot ce a fost citit de la butoane si afiseaza doar rezultatul calculului
+def calculate():
+    entire_string = display.get()
+    try:
+        a = parser.expr(entire_string).compile()
+        result = eval(a)
+        clear_all()
+        display.insert(0,result)
+    except Exception:
+        clear_all()
+        display.insert(0,"Error")
+ 
+#aceasta functie recunoaste operatorul si il afiseaza
+def get_operation(operator):
+    global i
+    length = len(operator)
+    display.insert(i,operator)
+    i+=length
+ 
+#AC pentru a sterge tot 
+def clear_all():
+    display.delete(0,END)
+ 
+#stergerea ultimului caracter
+def undo():
+    entire_string = display.get()
+    if len(entire_string):
+        new_string = entire_string[:-1]
+        clear_all()
+        display.insert(0,new_string)
+    else:
+        clear_all()
+        display.insert(0,"Error")
+
+#functie pentru calcularea factorialului
+def fact():
+    entire_string = display.get()
+    try:
+        result = factorial(int(entire_string))
+        clear_all()
+        display.insert(0,result)
+    except Exception:
+        clear_all()
+        display.insert(0,"Eroare")
+
+#partea de frontend a calculatorului
+
+#adaugarea campului unde vor aparea datele
+display = Entry(root)
+display.grid(row=1,columnspan=6,sticky=N+E+W+S)
+
+#schimbarea fundalului (am reusit doar la display)
+
+display.configure(background="orange") 
+ 
+#adaugarea butoanelor cu numere
+
+Button(root,text="1",command = lambda :get_variables(1)).grid(row=2,column=0, sticky=N+S+E+W)
+Button(root,text=" 2",command = lambda :get_variables(2)).grid(row=2,column=1, sticky=N+S+E+W)
+Button(root,text=" 3",command = lambda :get_variables(3)).grid(row=2,column=2, sticky=N+S+E+W)
+
+Button(root,text="4",command = lambda :get_variables(4)).grid(row=3,column=0, sticky=N+S+E+W)
+Button(root,text=" 5",command = lambda :get_variables(5)).grid(row=3,column=1, sticky=N+S+E+W)
+Button(root,text=" 6",command = lambda :get_variables(6)).grid(row=3,column=2, sticky=N+S+E+W)
+ 
+Button(root,text="7",command = lambda :get_variables(7)).grid(row=4,column=0, sticky=N+S+E+W)
+Button(root,text=" 8",command = lambda :get_variables(8)).grid(row=4,column=1, sticky=N+S+E+W)
+Button(root,text=" 9",command = lambda :get_variables(9)).grid(row=4,column=2, sticky=N+S+E+W)
+ 
+#adaugarea butoanelor cu functii simple
+Button(root,text="AC",command=lambda :clear_all()).grid(row=5,column=0, sticky=N+S+E+W)
+Button(root,text=" 0",command = lambda :get_variables(0)).grid(row=5,column=1, sticky=N+S+E+W)
+Button(root,text=" .",command=lambda :get_variables(".")).grid(row=5, column=2, sticky=N+S+E+W)
+ 
+ 
+Button(root,text="+",command= lambda :get_operation("+")).grid(row=2,column=3, sticky=N+S+E+W)
+Button(root,text="-",command= lambda :get_operation("-")).grid(row=3,column=3, sticky=N+S+E+W)
+Button(root,text="",command= lambda :get_operation("")).grid(row=4,column=3, sticky=N+S+E+W)
+Button(root,text="/",command= lambda :get_operation("/")).grid(row=5,column=3, sticky=N+S+E+W)
+ 
+# adaugarea butoanelor cu functii mai complexe
+Button(root,text="pi",command= lambda :get_operation("*3.14")).grid(row=2,column=4, sticky=N+S+E+W)
+Button(root,text="%",command= lambda :get_operation("%")).grid(row=3,column=4, sticky=N+S+E+W)
+Button(root,text="(",command= lambda :get_operation("(")).grid(row=4,column=4, sticky=N+S+E+W)
+Button(root,text="exp",command= lambda :get_operation("**")).grid(row=5,column=4, sticky=N+S+E+W)
+ 
+Button(root,text="<-",command= lambda :undo()).grid(row=2,column=5, sticky=N+S+E+W)
+Button(root,text="x!", command= lambda: fact()).grid(row=3,column=5, sticky=N+S+E+W)
+Button(root,text=")",command= lambda :get_operation(")")).grid(row=4,column=5, sticky=N+S+E+W)
+Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W)
+Button(root,text="^2",command= lambda :get_operation("**2")).grid(row=5,column=5, sticky=N+S+E+W)
+Button(root,text="=",command= lambda :calculate()).grid(columnspan=6, sticky=N+S+E+W)
+
+#nu am reusit sa lucrez cu procente pentru a-l face responsiv
+
+root.mainloop()
